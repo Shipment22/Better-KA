fc1fc0704f883cedd90c4458938b23d0064fac99
stranothus <stranothus@gmail.com>
2021-11-17 08:41:57 -0600

    Switch editor files Switch editor files when a tab is clicked. A data tab attribute is set for each tab element corresponding to the file to open when that tab is clicked. Different file editors are stored in an easily expandable object. Language mode is set based on the file extension.

--- editor/editor.js
+++ editor/editor.js
@@ -1,4 +1,24 @@
-var editor = ace.edit("editor");
+const editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
-editor.session.setMode("ace/mode/javascript");
-editor.setValue('heyeyyey')
+
+const EditSession = require("ace/edit_session").EditSession; // get EditSession
+
+var editors = {
+    "index.html": new EditSession(["<!DOCTYPE html>", "<html>", "\t<head>", "\t\t<meta charset = 'utf-8'>", "\t\t<title>New Webpage</title>", "\t</head>", "\t<body>", "\t\t", "\t</body>", "</html>"]),
+    "sketch.js": new EditSession(""),
+    "style.css": new EditSession(["* {", "\tmargin : 0;", "\tpadding : 0;", "}"])
+}; // store ACE tabs corresponding to their tab name. This way, new tabs can be added and old ones can be renamed dynamically
+
+for(let key in editors) {
+    editors[key].setMode("ace/mode/" + key.split(".").reverse()[0]);
+} // loop through the editor tabs and set the language mode based on file extension
+
+editor.setSession(editors["index.html"]); // set the current tab
+
+var tabs = document.querySelectorAll(".tab"); // get all .tab elements. querySelectorAll returns a forEachable
+
+tabs.forEach(element => {
+    element.addEventListener("click", event => {
+        editor.setSession(editors[element.dataset.tab]); // change the current edtor to the tab corresponding to the element clicked
+    });
+});
