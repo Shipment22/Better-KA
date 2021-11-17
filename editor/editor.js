31955252ef4ca6af47098e6d0cfe3b3eaa0dd42f
stranothus <stranothus@gmail.com>
2021-11-17 09:01:56 -0600

    Update filesystem when a .new-tab is created The filesystem is now based directly on the tabs present. When a new tab is added, provided it has a class of .new-tab, a matching file is created and linked to the tab. The .new-tab class is then removed so tabs don't become accidentally linked to a second file. A mutation observer is responsible for updating the files. It looks for child list updates from #tab which is the parent element for .tab elements. No other elements should go into #tab.

--- editor/editor.js
+++ editor/editor.js
@@ -3,22 +3,38 @@ editor.setTheme("ace/theme/monokai");
 
 const EditSession = require("ace/edit_session").EditSession; // get EditSession
 
-var editors = {
-    "index.html": new EditSession(["<!DOCTYPE html>", "<html>", "\t<head>", "\t\t<meta charset = 'utf-8'>", "\t\t<title>New Webpage</title>", "\t</head>", "\t<body>", "\t\t", "\t</body>", "</html>"]),
-    "sketch.js": new EditSession(""),
-    "style.css": new EditSession(["* {", "\tmargin : 0;", "\tpadding : 0;", "}"])
-}; // store ACE tabs corresponding to their tab name. This way, new tabs can be added and old ones can be renamed dynamically
+var editors = {}; // store ACE tabs corresponding to their tab name. This way, new tabs can be added and old ones can be renamed dynamically
 
-for(let key in editors) {
-    editors[key].setMode("ace/mode/" + key.split(".").reverse()[0]);
-} // loop through the editor tabs and set the language mode based on file extension
+function setEditorSwitch() {
+    var tabs = document.querySelectorAll(".new-tab"); // get all new tab elements. querySelectorAll returns a forEachable list
 
-editor.setSession(editors["index.html"]); // set the current tab
+    tabs.forEach(element => {
+        let extension = element.dataset.tab.split(".").reverse()[0]; // get file extension
 
-var tabs = document.querySelectorAll(".tab"); // get all .tab elements. querySelectorAll returns a forEachable
+        // create editor for file with starter code for certain file extensions
+        if(extension === "html") {
+            editors[element.dataset.tab] = new EditSession(["<!DOCTYPE html>", "<html>", "\t<head>", "\t\t<meta charset = 'utf-8'>", "\t\t<title>New Webpage</title>", "\t</head>", "\t<body>", "\t\t", "\t</body>", "</html>"]);
+        } else if(extension === "css") {
+            editors[element.dataset.tab] = new EditSession(["* {", "\tmargin: 0;", "\tpadding: 0;", "}"]);
+        } else {
+            editors[element.dataset.tab] = new EditSession("");
+        }
 
-tabs.forEach(element => {
-    element.addEventListener("click", event => {
-        editor.setSession(editors[element.dataset.tab]); // change the current edtor to the tab corresponding to the element clicked
+        editors[element.dataset.tab].setMode("ace/mode/" + extension); // set language mode depending on extension
+        
+        element.onclick = event => {
+            editor.setSession(editors[element.dataset.tab]); // change the current edtor to the tab corresponding to the element clicked
+        };
+
+        element.classList.remove("new-tab"); // remove new-tab class so the file isn't created again
     });
-});
+}
+
+// create a mutation observer to update the file system when a .new-tab is added
+const updateEditors = new MutationObserver(setEditorSwitch);
+const tabsCont = document.querySelector("#tabs");
+updateEditors.observe(tabsCont, { childList: true });
+
+// initial file setup
+setEditorSwitch();
+editor.setSession(editors["index.html"]); // set the current tab
