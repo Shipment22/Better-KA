e2e5215dea67d1c0f158eec2178acedfbad22e12
Shipment22 <crapple2@datazap.net>
2021-12-02 23:56:41 -0500

    :tada: Undo and redo support works!

--- editor/editor.js
+++ editor/editor.js
@@ -1,19 +1,48 @@
 const editor = ace.edit("editor");
+var UndoManager = require("ace/undomanager").UndoManager;
 
 editor.setOptions({
-    fontSize: '18px',
-    enableBasicAutocompletion: true,
-    enableLiveAutocompletion: true,
+	fontSize: '18px',
+	enableBasicAutocompletion: true,
+	enableLiveAutocompletion: true,
 });
 
 editor.commands.addCommand({
-    name: 'myCommand',
-    bindKey: {win: 'Ctrl-Enter',  mac: 'Command-Enter'},
-    exec: function(editor) {
-        startOutput();
-    },
-    readOnly: true, 
+	name: 'Run',
+	bindKey: 'Shift-Enter',
+	exec: function(editor) {
+		startOutput();
+	},
+	readOnly: true, 
 });
+
+editor.commands.addCommand({
+	name: 'New Line',
+	bindKey: {win: 'Ctrl-Enter',  mac: 'Command-Enter'},
+	exec: function(editor) {
+		// new line making code goes here...
+	},
+	readOnly: true, 
+});
+
+editor.commands.addCommands([
+	{
+		name : 'undo',
+		bindKey : { win: 'Ctrl-Z', mac: 'Command-Z' },
+		exec : function(editor){
+			editor.session.getUndoManager().undo();
+			console.log('undo')
+		}
+	},
+	{
+		name : 'redo',
+		bindKey : { win: 'Ctrl-Shift-Z', mac: 'Command-Shift-Z' },
+		exec : function(editor){
+			editor.session.getUndoManager().redo();
+			console.log('redo')
+		}
+	}
+]);
 
 
 editor.setTheme("ace/theme/monokai");
@@ -21,38 +50,41 @@ editor.setTheme("ace/theme/monokai");
 const EditSession = require("ace/edit_session").EditSession; // get EditSession
 
 var editors = {}; // store ACE tabs corresponding to their tab name. This way, new tabs can be added and old ones can be renamed dynamically
+var undoManagers = {}; // store undo managers, corresponding to their tab name aswell.
 
 function setEditorSwitch() {
-    var tabs = document.querySelectorAll(".new-tab"); // get all new tab elements. querySelectorAll returns a forEachable list
+	var tabs = document.querySelectorAll(".new-tab"); // get all new tab elements. querySelectorAll returns a forEachable list
 
-    tabs.forEach(element => {
-        let name = element.dataset.tab;                 // get file name
-        let extension = name.split(".").reverse()[0];   // get file extension
+	tabs.forEach(element => {
+		let name = element.dataset.tab;                 // get file name
+		let extension = name.split(".").reverse()[0];   // get file extension
 
 
-        // create editor for file with starter code for certain file names
-        if (name === 'index.html') {
-            editors[element.dataset.tab] = new EditSession(["<!DOCTYPE html>", "<html>", "\t<head>", "\t\t<meta charset = 'utf-8'>", '\t\t<link rel="stylesheet" type="text/css" href="style.css">', "\t\t<title>New Webpage</title>", "\t</head>", "\t<body>", "\t\t", '\t\t<script src="sketch.js"></script>', "\t</body>", "</html>"]);
-        } else {
-            // create editor for file with starter code for certain file extensions
-            if(extension === "html") {
-                editors[element.dataset.tab] = new EditSession(["<!DOCTYPE html>", "<html>", "\t<head>", "\t\t<meta charset = 'utf-8'>", "\t\t<title>New Webpage</title>", "\t</head>", "\t<body>", "\t\t", "\t</body>", "</html>"]);
-            } else if(extension === "css") {
-                editors[element.dataset.tab] = new EditSession(["* {", "\tmargin: 0;", "\tpadding: 0;", "}"]);
-            } else {
-                editors[element.dataset.tab] = new EditSession([""]);
-            }
-        }
-        
+		// create editor for file with starter code for certain file names
+		if (name === 'index.html') {
+			editors[element.dataset.tab] = new EditSession(["<!DOCTYPE html>", "<html>", "\t<head>", "\t\t<meta charset = 'utf-8'>", '\t\t<link rel="stylesheet" type="text/css" href="style.css">', "\t\t<title>New Webpage</title>", "\t</head>", "\t<body>", "\t\t", '\t\t<script src="sketch.js"></script>', "\t</body>", "</html>"]);
+		} else {
+			// create editor for file with starter code for certain file extensions
+			if(extension === "html") {
+				editors[element.dataset.tab] = new EditSession(["<!DOCTYPE html>", "<html>", "\t<head>", "\t\t<meta charset = 'utf-8'>", "\t\t<title>New Webpage</title>", "\t</head>", "\t<body>", "\t\t", "\t</body>", "</html>"]);
+			} else if(extension === "css") {
+				editors[element.dataset.tab] = new EditSession(["* {", "\tmargin: 0;", "\tpadding: 0;", "}"]);
+			} else {
+				editors[element.dataset.tab] = new EditSession([""]);
+			}
+		}
+		
+		undoManagers[element.dataset.tab] = new UndoManager();
+		editors[element.dataset.tab].setUndoManager(undoManagers[element.dataset.tab]);
 
-        editors[element.dataset.tab].setMode("ace/mode/" + extension); // set language mode depending on extension
-        
-        element.onclick = event => {
-            editor.setSession(editors[element.dataset.tab]); // change the current edtor to the tab corresponding to the element clicked
-        };
+		editors[element.dataset.tab].setMode("ace/mode/" + extension); // set language mode depending on extension
+		
+		element.onclick = event => {
+			editor.setSession(editors[element.dataset.tab]); // change the current edtor to the tab corresponding to the element clicked
+		};
 
-        element.classList.remove("new-tab"); // remove new-tab class so the file isn't created again
-    });
+		element.classList.remove("new-tab"); // remove new-tab class so the file isn't created again
+	});
 }
 
 // create a mutation observer to update the file system when a .new-tab is added
@@ -65,66 +97,66 @@ setEditorSwitch();
 editor.setSession(editors["index.html"]); // set the current tab
 
 function addTab(title) {
-    if (title === '' || title === ' ') {
-        addTab(prompt('That name does not start with a letter/number, please enter a new one.'));
-        return;
-    } else if (editors[title] !== undefined) {  // makes sure that you name it something different from everything else
-        addTab(prompt('That filename has already been used, please enter a new one.'));
-        return;
-    }
+	if (title === '' || title === ' ') {
+		addTab(prompt('That name does not start with a letter/number, please enter a new one.'));
+		return;
+	} else if (editors[title] !== undefined) {  // makes sure that you name it something different from everything else
+		addTab(prompt('That filename has already been used, please enter a new one.'));
+		return;
+	}
 
-    let tab = document.createElement('span');   // create tab
+	let tab = document.createElement('span');   // create tab
 
-    tab.classList.add('tab');                   // set classes
-    tab.classList.add('new-tab');
+	tab.classList.add('tab');                   // set classes
+	tab.classList.add('new-tab');
 
-    tab.dataset.tab = title;                    // set content and dataset.tab to the title
-    tab.textContent = title;
+	tab.dataset.tab = title;                    // set content and dataset.tab to the title
+	tab.textContent = title;
 
-    tabsCont.appendChild(tab);                  // append to the tabs bar
+	tabsCont.appendChild(tab);                  // append to the tabs bar
 
-    setTimeout(function(){                      // wait 10 milliseconds then set the current tab
-        editor.setSession(editors[title]);      
-    }, 10);
+	setTimeout(function(){                      // wait 10 milliseconds then set the current tab
+		editor.setSession(editors[title]);      
+	}, 10);
 }
 
 const output = document.querySelector('#output');
 
 function stopOutput() {
-    output.srcdoc = '';
+	output.srcdoc = '';
 }
 stopOutput();
 
 function startOutput() {
-    stopOutput();
+	stopOutput();
 
-    let tabs = document.querySelectorAll('.tab');
+	let tabs = document.querySelectorAll('.tab');
 
-    let html = editors['index.html'].getValue();    // gets html code
+	let html = editors['index.html'].getValue();    // gets html code
 
-    tabs.forEach(element => {
-        let name = element.dataset.tab;            // get name and extension
-        let extension = name.split('.').reverse()[0];
+	tabs.forEach(element => {
+		let name = element.dataset.tab;            // get name and extension
+		let extension = name.split('.').reverse()[0];
 
-        function r(replace, what) {
-            html = html.replaceAll(replace, what);
-        }
+		function r(replace, what) {
+			html = html.replaceAll(replace, what);
+		}
 
-        if (extension === 'css') {
-            r(`<link rel="stylesheet" type="text/css" href="${name}">`, `<style>${editors[name].getValue()}</style>`);
-            r(`<link type="text/css" rel="stylesheet" href="${name}">`, `<style>${editors[name].getValue()}</style>`);
-        } else if (extension === 'js') {
-            r(`src="${name}">`, `>${editors[name].getValue()}`);
-        } else if (extension === 'html') {
-            // something goes here...
-        } else {
-            console.error('unknown file extension');
-        }
-    });
+		if (extension === 'css') {
+			r(`<link rel="stylesheet" type="text/css" href="${name}">`, `<style>${editors[name].getValue()}</style>`);
+			r(`<link type="text/css" rel="stylesheet" href="${name}">`, `<style>${editors[name].getValue()}</style>`);
+		} else if (extension === 'js') {
+			r(`src="${name}">`, `>${editors[name].getValue()}`);
+		} else if (extension === 'html') {
+			// something goes here...
+		} else {
+			console.error('unknown file extension');
+		}
+	});
 
-    output.srcdoc = html;
+	output.srcdoc = html;
 }
 
 function saveCode() {
-    // something goes here...
+	// something goes here...
 }
